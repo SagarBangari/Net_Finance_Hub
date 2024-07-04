e296834c45bbd5b952e9cd858e6622f3fa596481
Sagar I Bangari <122220877+SagarBangari@users.noreply.github.com>
2024-07-04 19:33:52 +0530

    Update login_screen.dart

--- lib/screens/auth/email_auth/login_screen.dart
+++ lib/screens/auth/email_auth/login_screen.dart
@@ -5,6 +5,7 @@ import '../../../main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'forgot_password.dart';
+import 'package:email_validator/email_validator.dart';
 class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
 
@@ -18,6 +19,7 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passwordController = TextEditingController();
   bool isLoading = false;
   String error = '';
+  bool _isEmailValid = false;
   void login() async {
     setState(() {
       isLoading = true;
@@ -25,10 +27,19 @@ class _LoginScreenState extends State<LoginScreen> {
     });
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
-
-    if(email == "" || password == "") {
+   RegExp emailRegex = RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$');
+   bool isValidEmail = emailRegex.hasMatch(email);
+   if(!isValidEmail){
+     isLoading = false;
+      error="Enter valid email";
+   }
+   else  if(email == "" || password == "") {
       isLoading = false;
       error="Please fill all the fields!";
+    }
+    else if(password.length <6){
+      isLoading = false;
+      error = "password should be at least 6 charecters long ";
     }
     else {
 
@@ -44,12 +55,26 @@ class _LoginScreenState extends State<LoginScreen> {
             builder: (context) => HomeNavigationPage(0)
           ));
           
-        }else{
-          isLoading = false;
-          error = 'The user is not registered';
         }
         
       } on FirebaseAuthException catch(ex) {
+        setState(() {
+          isLoading = false;
+          switch (ex.code) {
+            case 'user-not-found':
+              error = 'No user found for that email.';
+              break;
+            case 'wrong-password':
+              error = 'Wrong password provided for that user.';
+              break;
+            case 'invalid-email':
+              error = 'The email address is not valid.';
+              break;
+            default:
+              error = 'Wrong password provided for that user';
+              break;
+          }
+        });
         log(ex.code.toString());
       }
     }
@@ -78,6 +103,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       labelText: "Email Address",
                       border: const OutlineInputBorder(),
                     ),
+                   
                   ),
 
                   SizedBox(height: 10,),
